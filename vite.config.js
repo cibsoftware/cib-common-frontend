0716a974d44b382f4cd366d7e9c90dfcca920f44
Serge Krot <serge.krot@cib.de>
2025-12-04 15:24:01 +0100

    chore: update package.json and vite.config.js for improved configuration and dependencies
    - Updated package.json to change the main and module entry points, added style and files fields, and modified test scripts for better coverage reporting.
    - Added vue-devtools plugin to vite.config.js for enhanced development experience.
    - Updated build configuration in vite.config.js to reflect new file naming conventions.

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,39 @@
+/*
+ * Copyright CIB software GmbH and/or licensed to CIB software GmbH
+ * under one or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information regarding copyright
+ * ownership. CIB software licenses this file to you under the Apache License,
+ * Version 2.0; you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 import { fileURLToPath, URL } from 'node:url'
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
 import path from 'path'
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
+// https://vite.dev/config/
 export default defineConfig({
   base: '/common-frontend/',
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    vueDevTools(),
+  ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      vue: 'vue/dist/vue.esm-bundler.js'
     },
   },
   build: {
@@ -18,7 +41,8 @@ export default defineConfig({
     lib: {
       entry: path.resolve(__dirname, 'src/library.js'),
       name: 'CibCommonFrontend',
-      fileName: (format) => `cib-common-frontend.${format}.js`
+      formats: ['es', 'umd'],
+      fileName: (format) => `common-frontend.${format}.js`
     },
     rollupOptions: {
       external: ['vue', 'bootstrap', 'vue-i18n', 'vue-router'],
