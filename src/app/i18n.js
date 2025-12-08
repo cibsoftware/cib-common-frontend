84c55b9acca320a5345ec0470f0a3a12fb9d13d8
Serge Krot <serge.krot@cib.de>
2025-12-08 15:52:54 +0100

    feat(i18n): add internationalization setup with translation files

---
+++ src/app/i18n.js
@@ -0,0 +1,42 @@
+/*
+ * Copyright CIB software GmbH and/or licensed to CIB software GmbH
+ * under one or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information regarding copyright
+ * ownership. CIB software licenses this file to you under the Apache License,
+ * Version 2.0; you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+import { createI18n } from 'vue-i18n'
+
+// Import translation files
+import translationsDe from '../assets/translations_de.json'
+import translationsEn from '../assets/translations_en.json'
+import translationsEs from '../assets/translations_es.json'
+import translationsIt from '../assets/translations_it.json'
+import translationsRu from '../assets/translations_ru.json'
+import translationsUa from '../assets/translations_ua.json'
+
+// Create and export i18n instance
+export const i18n = createI18n({
+  locale: 'en', // Default locale
+  fallbackLocale: 'en',
+  messages: {
+    de: translationsDe,
+    en: translationsEn,
+    es: translationsEs,
+    it: translationsIt,
+    ru: translationsRu,
+    ua: translationsUa
+  }
+})
+
+export default i18n
